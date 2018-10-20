070357161262c5005e2d2114270fe95a08949d20
Suji <sujeongji.suji@gmail.com>
2018-10-20 18:31:33 +0900

    181019

---
+++ prep01/src/solution/ToLowerCase.java
@@ -0,0 +1,25 @@
+package solution;
+
+public class ToLowerCase {
+    public String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+
+
+	public static void main(String args[]) {
+	    String str = "HELLO";
+	    String strlower = str.toLowerCase();
+	    System.out.println(strlower); 
+ }
+}
+
+/*
+public class ToLowerCase {
+
+   public static void main(String args[]) {
+      String str = "HELLO";
+      String strlower = str.toLowerCase();
+      System.out.println(strlower); 
+   }
+}
+*/
